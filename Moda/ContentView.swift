22437dcb94ac845c251ea5c9edeca6e80ea4b31b
금가경 <gageum0@gmail.com>
2025-11-06 15:57:25 +0900

    git 초기화 및 gitignore 추가

---
+++ Moda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moda
+//
+//  Created by 금가경 on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moda/ModaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModaApp.swift
+//  Moda
+//
+//  Created by 금가경 on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ModaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
